41f5a6cbb06f0ffb1b2c8284d1460f1c791d3286
tamersharkas <107429812+tamersharkas@users.noreply.github.com>
2022-06-21 21:59:00 +0200

    Update auctions-for-woocommerce.php

--- auctions-for-woocommerce/auctions-for-woocommerce.php
+++ auctions-for-woocommerce/auctions-for-woocommerce.php
@@ -14,7 +14,7 @@
  * Plugin Name:       Auctions for WooCommerce
  * Plugin URI:        https://wpinstitut.com/auctions-for-woocommerce/
  * Description:       Easily extend WooCommerce with auction features and functionalities.
- * Version:           2.7
+ * Version:           3.7
  * Author:            WPInstitut
  * Author URI:        https://wpinstitut.com/
  * License:           GPL-2.0+
